b498c66879964bc33bf7ece2a2ef30fe7199f510
MartinSVK12 <37455793+MartinSVK12@users.noreply.github.com>
2024-06-11 17:26:52 +0200

    Fix crash.

--- src/main/java/sunsetsatellite/retrostorage/gui/GuiDigitalFluid.java
+++ src/main/java/sunsetsatellite/retrostorage/gui/GuiDigitalFluid.java
@@ -183,13 +183,13 @@ public class GuiDigitalFluid extends GuiFluid {
                 int waterColor = BlockColorDispatcher.getInstance().getDispatch(Block.fluidWaterFlowing).getWorldColor(this.mc.theWorld, (int) this.mc.thePlayer.x, (int) this.mc.thePlayer.y, (int) this.mc.thePlayer.z);
                 Color c = new Color().setARGB(waterColor);
                 c.setRGBA(c.getRed(),c.getGreen(),c.getBlue(),0x40);
-                ((IColorOverride)itemModel).overrideColor(c.getRed(),c.getGreen(),c.getBlue(),c.getAlpha());
+                
                 ((IColorOverride)blockModel).overrideColor(c.getRed(),c.getGreen(),c.getBlue(),c.getAlpha());
                 itemModel.renderItemIntoGui(Tessellator.instance,this.fontRenderer, this.mc.renderEngine, itemStack4, i2, i3,1.0F);
             } else {
                 itemModel.renderItemIntoGui(Tessellator.instance,this.fontRenderer, this.mc.renderEngine, itemStack4, i2, i3,1.0F);
             }
-            ((IColorOverride)itemModel).overrideColor(1,1,1,1);
+            
             ((IColorOverride)blockModel).overrideColor(1,1,1,1);
             itemModel.renderItemOverlayIntoGUI(Tessellator.instance,this.fontRenderer, this.mc.renderEngine, itemStack4, i2, i3, NumberUtil.format(slot1.getFluidStack().amount), 1.0F);
         }
